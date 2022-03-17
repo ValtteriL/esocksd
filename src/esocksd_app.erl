6d34adcd1f2b213b425271faa7e03a6f4d662a30
Valtteri <valtteri@shufflingbytes.com>
2022-03-17 18:37:16 +0200

    create app

---
+++ src/esocksd_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc esocksd public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(esocksd_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    esocksd_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
