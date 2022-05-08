4a18cf4b9b989c3693c7eddbe1c755dd94932ef1
Valtteri <valtteri@shufflingbytes.com>
2022-05-08 17:54:32 +0300

    add specs to exported funcs in helpers

--- src/helpers.erl
+++ src/helpers.erl
@@ -4,6 +4,7 @@
 -export([bytes_to_addr/1, addr_to_bytes/1, integer_to_2byte_binary/1, bytes_to_atyp/1, resolve/1]).
 
 % convert bytes into tuple representation of IP address (tuple)
+-spec bytes_to_addr(binary()) -> tuple().
 bytes_to_addr(Bytes) ->
     case byte_size(Bytes) of
         4 ->
@@ -13,6 +14,7 @@ bytes_to_addr(Bytes) ->
             bytes_to_ipv6_addr(Bytes)
     end.
 
+-spec addr_to_bytes(tuple()) -> binary().
 addr_to_bytes(Addr) when tuple_size(Addr) == 4 ->
     binary:list_to_bin(tuple_to_list(Addr));
 addr_to_bytes(Addr) when tuple_size(Addr) == 8 ->
@@ -30,6 +32,7 @@ bytes_to_ipv6_addr(Acc, <<>>) ->
     list_to_tuple(Acc).
 
 % convert integer to 2-byte unsigned binary
+-spec integer_to_2byte_binary(integer()) -> binary().
 integer_to_2byte_binary(Integer) ->
     Bytes = binary:encode_unsigned(Integer),
     case byte_size(Bytes) of
@@ -41,6 +44,7 @@ integer_to_2byte_binary(Integer) ->
 
 
 % given IP address bytes figure address type by bytes
+-spec bytes_to_atyp(binary()) -> byte().
 bytes_to_atyp(Bytes) ->
     case byte_size(Bytes) of
         4 ->
@@ -53,6 +57,7 @@ bytes_to_atyp(Bytes) ->
 
 
 % resolve domain
+-spec resolve(string()) -> tuple().
 resolve(Domain) ->
     {ok,{hostent,_,_,_,_,[Addr|_]}} = inet:gethostbyname(Domain), % resolve name to ipv4/ipv6 address
     Addr.
