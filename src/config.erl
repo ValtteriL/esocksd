fbaf99e7e925288513831a8f06da55f9f5007d66
Valtteri <valtteri@shufflingbytes.com>
2022-05-14 13:44:41 +0300

    prettify shell output

--- src/config.erl
+++ src/config.erl
@@ -46,6 +46,7 @@ load() ->
     % set logging settings
     LogLevel = lookup_ets(loglevel),
     LogFile = lookup_ets(logfile),
+    logger:update_handler_config(default, formatter, {logger_formatter, #{}}), % set defaults for default logger
     logger:set_primary_config(level, LogLevel), % set loglevel globally
     Config = #{config => #{file => LogFile}, level => LogLevel},
     logger:add_handler(myhandler, logger_std_h, Config),
