7c53e3aeb682c85243c56798eeb17bb9945c3159
Valtteri <valtteri@shufflingbytes.com>
2022-05-14 13:58:16 +0300

    move eunit tests to ct

---
+++ test/eunit_SUITE.erl
@@ -0,0 +1,10 @@
+-module(eunit_SUITE).
+-include_lib("common_test/include/ct.hrl").
+ 
+-export([all/0]).
+-export([run_eunit/1]).
+ 
+all() -> [run_eunit].
+
+run_eunit(_Config) ->
+    ok = eunit:test().
